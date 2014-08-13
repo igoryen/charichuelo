7e22a4998a91109b578005e547ac724751a43b32
igoryen <igor.yentaltsev@gmail.com>
2014-08-13 19:03:06 -0400

    Altered charichuelo.xsd, CharichueloWS.java

--- src/charichuelo/CharichueloWS.java
+++ src/charichuelo/CharichueloWS.java
@@ -13,22 +13,34 @@ import javax.jws.soap.SOAPBinding;
     parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
 public class CharichueloWS {
   
+  ObjectFactory objFactory = new ObjectFactory();
+  // CustomerSearch()
+  
   @WebMethod
   @WebResult(name = "CustomerSearchResponse")
   public CustomerSearchResponse mthdCustomerSearch(@WebParam(name = "CustomerSearchRequest") CustomerSearchRequest request) {
-    ObjectFactory objFactory = new ObjectFactory();
     CustomerSearchResponse response = objFactory.createCustomerSearchResponse();
     response.setCstCustomerId(request.getCstCustomerId());
     return response;
   }
   
+  // CustomerOrderSearch()
+  
   @WebMethod
   @WebResult(name = "CustomerOrderSearchResponse")
   public CustomerOrderSearchResponse mthdCustomerOrderSearch(@WebParam(name = "CustomerOrderSearchRequest") CustomerOrderSearchRequest request) {
-    ObjectFactory objFactory = new ObjectFactory();
     CustomerOrderSearchResponse response = objFactory.createCustomerOrderSearchResponse();
     response.setCorOrderId(request.getCorOrderId());
     return response;
   }
   
+  // ProductCreate()
+  
+  @WebMethod
+  @WebResult(name = "ProductCreateResponse")
+  public ProductCreateResponse mthdProductCreate(@WebParam(name = "ProductCreateRequest") ProductCreateRequest request) {
+    ProductCreateResponse response = objFactory.createProductCreateResponse();
+    response.setPrdProductId(request.getPrdProductId());
+    return response;
+  }
 }
